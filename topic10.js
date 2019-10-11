a2b2164a56528adec13d42741e1136253e3579f5
Ashleigh-Walter <sho09010@byui.edu>
2019-10-11 15:11:02 -0400

    file added

--- topic10.js
+++ topic10.js
@@ -15,11 +15,11 @@ ctx.strokeStyle = 'pink';
 ctx.stroke();
 
 // locate the canvas element
-var canvas = document.getElementById("myCanvas2");
+var canvas2 = document.getElementById("myCanvas2");
 // create a drawing object
 // getContext() is a built in html object with methods and properties
 // for drawing
-var ctx = canvas.getContext("2d");
+var ctx = canvas2.getContext("2d");
 // the fillStyle property fills in the object
 // can be a color, gradient or pattern
 ctx.fillStyle = "#FF0000";
@@ -27,9 +27,9 @@ ctx.fillStyle = "#FF0000";
 // fillStyle
 ctx.fillRect(50, 50, 100, 100);
 
-var canvas = document.getElementById("myCanvas3");
-var ctx=canvas.getContext("2d");
+var canvas3 = document.getElementById("myCanvas3");
+var ctx=canvas3.getContext("2d");
 ctx.font="30px Comic Sans MS";
 ctx.fillStyle = "white";
 ctx.textAlign = "center";
-ctx.fillText("Check out my text!", canvas.width/2, canvas.height/2);
+ctx.fillText("Check out my text!", canvas3.width/2, canvas3.height/2);
